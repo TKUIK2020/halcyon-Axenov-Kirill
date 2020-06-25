a5f981f43f046364f1f1db01d76ff0fc466217bd
My Name <myEmail@example.com>
2020-06-25 20:53:23 +0300

    Add project

---
+++ Лаб 13/dist/js.js
@@ -0,0 +1,9 @@
+function viewBlock() {
+  const modWin = document.getElementById('modWin');
+  modWin.removeAttribute('hidden');
+}
+
+function hideBlock() {
+  const modWin = document.getElementById('modWin');
+  modWin.setAttribute('hidden', '');
+}
